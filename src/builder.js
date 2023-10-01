aa6ad59c20c1985084c73f2edeaedc5aa2f31f5d
Newalfox <52476035+Alane38@users.noreply.github.com>
2023-10-01 03:11:18 +0200

    movable object

--- src/builder.js
+++ src/builder.js
@@ -1,7 +1,5 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
-import { FontLoader } from "three/addons/loaders/FontLoader.js";
 
 // Objects imports
 import { Cube } from "./objects/cube";
@@ -51,7 +49,6 @@ text
     console.error("An error was detected to load the font", error);
   });
 
-
 /**
  * Lightning
  */
@@ -80,6 +77,24 @@ scene.add(camera);
 
 // Controls with mouse
 const controls = new OrbitControls(camera, canvas);
+
+controls.keys = {
+  // Move
+  LEFT: "ArrowLeft", //left arrow
+  UP: "ArrowUp", // up arrow
+  RIGHT: "ArrowRight", // right arrow
+  DOWN: "ArrowDown", // down arrow
+
+  Z: "z",
+  Q: "q",
+  S: "s",
+  D: "d",
+
+  // Rotate
+  R: "r",
+  T: "t"
+};
+
 // controls.enabled = false
 controls.enableDamping = true;
 
@@ -101,28 +116,33 @@ const tick = () => {
   // console.log(elapsedTime)
 
   // Animate
-//   cube.setMeshRotations(
-//     cube.mesh.rotation.x,
-//     (cube.mesh.rotation.y += elapsedTime),
-//     cube.mesh.rotation.z
-//   );
+  //   cube.setMeshRotations(
+  //     cube.mesh.rotation.x,
+  //     (cube.mesh.rotation.y += elapsedTime),
+  //     cube.mesh.rotation.z
+  //   );
 
-  cube.setMeshPositions(null, null, elapsedTime)
+  //   cube.setMeshPositions(null, null, elapsedTime);
 
   text.setMeshPositions(
     cube.mesh.position.x - 1.8,
     cube.mesh.position.y + 1,
-    cube.mesh.position.z 
-    )
-    
+    cube.mesh.position.z
+  );
 
-//   cube.material.color.setRGB(
-//     Math.random(255),
-//     Math.random(255),
-//     Math.random(255)
-//   );
+  text.setMeshRotations(
+    cube.mesh.rotation.x,
+    cube.mesh.rotation.y,
+    cube.mesh.rotation.z
+  );
 
-    console.log(cube.getMeshPositions());
+  //   cube.material.color.setRGB(
+  //     Math.random(255),
+  //     Math.random(255),
+  //     Math.random(255)
+  //   );
+
+  // console.log(cube.getMeshPositions());
 
   // Update controls mouse
   controls.update();
@@ -137,9 +157,105 @@ const tick = () => {
 tick();
 
 /**
+ * Keyboard Events
+ */
+// Move & Rotate
+const movementState = {
+    // Move
+  UP: false,
+  DOWN: false,
+  LEFT: false,
+  RIGHT: false,
+
+  // Rotate
+  R: false,
+  T: false
+};
+
+// Move
+window.addEventListener("keydown", (event) => {
+  if (event.key == controls.keys["UP"] || event.key == controls.keys["Z"]) {
+    movementState.UP = true;
+  }
+  if (event.key == controls.keys["DOWN"] || event.key == controls.keys["S"]) {
+    movementState.DOWN = true;
+  }
+  if (event.key == controls.keys["LEFT"] || event.key == controls.keys["Q"]) {
+    movementState.LEFT = true;
+  }
+  if (event.key == controls.keys["RIGHT"] || event.key == controls.keys["D"]) {
+    movementState.RIGHT = true;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  if (event.key == controls.keys["UP"] || event.key == controls.keys["Z"]) {
+    movementState.UP = false;
+  }
+  if (event.key == controls.keys["DOWN"] || event.key == controls.keys["S"]) {
+    movementState.DOWN = false;
+  }
+  if (event.key == controls.keys["LEFT"] || event.key == controls.keys["Q"]) {
+    movementState.LEFT = false;
+  }
+  if (event.key == controls.keys["RIGHT"] || event.key == controls.keys["D"]) {
+    movementState.RIGHT = false;
+  }
+});
+
+// Rotate
+window.addEventListener("keydown", (event) => {
+  if (event.key == controls.keys["R"]) {
+    movementState.R = true;
+  }
+  if (event.key == controls.keys["T"]) {
+    movementState.T = true;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+    if (event.key == controls.keys["R"]) {
+      movementState.R = false;
+    }
+    if (event.key == controls.keys["T"]) {
+      movementState.T = false;
+    }
+  });
+
+// Make interaction result
+function updateCubePosition() {
+  const moveSpeed = 0.1; // Vitesse de déplacement
+  const rotateSpeed = 0.1; // Vitesse de déplacement
+
+  if (movementState.UP) {
+    cube.setMeshPositions(null, null, (cube.mesh.position.z -= moveSpeed));
+  }
+  if (movementState.DOWN) {
+    cube.setMeshPositions(null, null, (cube.mesh.position.z += moveSpeed));
+  }
+  if (movementState.LEFT) {
+    cube.setMeshPositions((cube.mesh.position.x -= moveSpeed), null, null);
+  }
+  if (movementState.RIGHT) {
+    cube.setMeshPositions((cube.mesh.position.x += moveSpeed), null, null);
+  }
+  if (movementState.R) {
+    cube.setMeshRotations(null,  null,(cube.mesh.rotation.x -= rotateSpeed));
+  }
+  if (movementState.T) {
+    cube.setMeshRotations((cube.mesh.rotation.x += rotateSpeed), null, null);
+  }
+
+  // Appelez la fonction à nouveau pour obtenir un mouvement fluide
+  requestAnimationFrame(updateCubePosition);
+}
+
+// Démarrez la mise à jour de la position du cube
+updateCubePosition();
+
+/**
  * Resize Window
  */
-
 function onWindowResize(justAdjustWidth) {
   const height = justAdjustWidth ? initialWindowHeight : window.innerHeight;
   const width = window.innerWidth;
